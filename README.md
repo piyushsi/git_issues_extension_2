a8810a7e19e93b520931400430f5e4c503436a45
piyushsi <piyushsinha2309@gmail.com>
2020-03-11 18:24:22 +0530

    add

---
+++ README.md
@@ -0,0 +1,13 @@
+
+## How to install
+
+1.Clone this repository
+``` git
+$ git clone 
+```
+
+2.Open [chrome://extensions/](chrome://extensions/)
+
+3.Check the box for **Developer Mode** in the top right.
+
+4.Click **Load unpacked extension** button and select `Youtube-Downloader-Extension` folder to install it.
---
+++ src/loader.js
@@ -0,0 +1,36 @@
+setTimeout(function() {
+  arr = [];
+  arr = Array.from(document.querySelectorAll(".Box-row")).map(a => {
+    return a.innerText.split("#")[1][0];
+  });
+  arr.map(n => {
+    let req = new XMLHttpRequest();
+    fetch(`${document.location.href}/${n}`).then(function(response) {
+      switch (response.status) {
+        // status "OK"
+        case 200:
+          var data = "";
+          var a = response.text().then(function(res) {
+            return res;
+          });
+          a.then(d => {
+            return (a = `${d}`);
+          });
+          setTimeout(function() {
+            data = a;
+            var h3 = document.createElement("span");
+            h3.className = "piyush";
+            h3.textContent = data
+              .split("160px;")[1]
+              .split(">")[1]
+              .split("</span")[0];
+            document.getElementById(`issue_${n}`).append(h3);
+          }, 500);
+
+        // status "Not Found"
+        case 404:
+          throw response;
+      }
+    });
+  });
+}, 100);
