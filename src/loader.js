4e0c45cef4c98382cdfa6386ab704139e9b549aa
piyushsi <piyushsinha2309@gmail.com>
2020-03-12 18:35:41 +0530

    final  refactor

--- src/loader.js
+++ src/loader.js
@@ -1,32 +1,26 @@
-setTimeout(function() {
-  arr = [];
+function styleIssue(data, n) {
+  var h3 = document.createElement("span");
+  h3.className = "piyush";
+  h3.textContent = data
+    .split("160px;")[1]
+    .split(">")[1]
+    .split("</span")[0];
+  document.getElementById(`issue_${n}`).append(h3);
+}
+function github() {
   arr = Array.from(document.querySelectorAll(".Box-row")).map(a => {
     return a.innerText.split("#")[1][0];
   });
   arr.map(n => {
-    let req = new XMLHttpRequest();
-    fetch(`${document.location.href}/${n}`).then(function(response) {
-      switch (response.status) {
-        case 200:
-          var data = response.text().then(function(res) {
-            return res;
-          });
-          data.then(d => {
-            return (data = `${d}`);
-          });
-          setTimeout(function() {
-            var h3 = document.createElement("span");
-            h3.className = "piyush";
-            if(data.split("160px;")[1]!=undefined){
-              h3.textContent = data
-              .split("160px;")[1]
-              .split(">")[1]
-              .split("</span")[0];
-            document.getElementById(`issue_${n}`).append(h3);
-            }  
-          }, 1000);
-        case 404:
-      }
+    fetch(`${document.location.href}/${n}`).then(function (response) {
+      var data = response.text().then(function (res) {
+        return res;
+      });
+      data.then(d => {
+        return (data = `${d}`);
+      })
+        .then(run => this.styleIssue(data, n))
     });
   });
-}, 100);
+}
+window.github()
