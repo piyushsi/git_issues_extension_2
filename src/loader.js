3633427d4d43282dd2ad2807fc73805dc91d050a
piyushsi <piyushsinha2309@gmail.com>
2020-03-17 02:59:39 +0530

    completed

--- src/loader.js
+++ src/loader.js
@@ -1,5 +1,28 @@
-function styleIssue(data, n) {
-  var h3 = document.createElement("span");
+
+var a=  fetch(
+    `${document.location.href.split("?")[0].split("issues")[0] + "projects"}`
+  ).then(function(response) {
+    var projectsData = response.text().then(function(res) {
+      return res;
+    });
+    projectsData
+      .then(d => {
+        return (projectsData = `${d}`);
+      })
+      .then(run =>
+         a = projectsData.split(`<h4 class="mb-1">`).map(a => {
+          if (a.split(`" class="link-gray-dark mr-1">`)[1] != undefined) {
+            return [
+              a.split(`" class="link-gray-dark mr-1">`)[1].split("</a>")[0],
+              a.split(`" class="link-gray-dark mr-1">`)[0].split("projects/")[1]
+            ];
+          }
+        })
+      )
+  });
+
+function styleIssue(data, n) {  
+  var h3 = document.createElement("a");
   h3.className = "issues_project";
   count = 0;
 
@@ -15,23 +38,45 @@ function styleIssue(data, n) {
     });
 
   if (count === 1) {
-    h3.textContent = data
+
+    res = data
       .split("160px;")[1]
       .split(">")[1]
       .split("</span")[0];
-    document.getElementById(`issue_${n}`).append(h3);
+    var def = document.getElementById(`issue_${n}_link`).innerHTML;
+    let num;
+    a.forEach(a=>{
+      if(a==undefined){
+       
+      }
+      else if (a[0]==res){
+        num = a[1];
+      }
+    })
+    var href = document.location.href.split("?")[0]+"?q=is%3Aopen+is%3Aissue+project%3A"+document.location.href.split("?")[0].split('/')[3]+"%2F"+document.location.href.split("?")[0].split('/')[4]+"%2F"+`${num}`;
+    document.getElementById(
+      `issue_${n}_link`
+    ).innerHTML = `[<a class="link-gray-dark v-align-middle no-underline h4 js-navigation-open" href=${href}>${res}</a>] ${def}`;
   } else if (count > 1) {
     for (var i = 1; i < count + 1; i++) {
-      var a = document.createElement("span");
-      a.className = "issues_project";
-      a.innerText = `${
-        data
-          .split("160px;")
-          [i].split(">")[1]
-          .split("</span>")[0]
-          .split("</span")[0]
-      }`;
-      document.getElementById(`issue_${n}`).append(a);
+      res = data
+        .split("160px;")
+        [i].split(">")[1]
+        .split("</span")[0];
+      var def = document.getElementById(`issue_${n}_link`).innerHTML;
+      let num;
+      a.forEach(a=>{
+        if(a==undefined){
+         
+        }
+        else if (a[0]==res){
+          num = a[1];
+        }
+      })
+    var href = document.location.href.split("?")[0]+"?q=is%3Aopen+is%3Aissue+project%3A"+document.location.href.split("?")[0].split('/')[3]+"%2F"+document.location.href.split("?")[0].split('/')[4]+"%2F"+`${num}`;
+      document.getElementById(
+        `issue_${n}_link`
+      ).innerHTML = `[<a class="link-gray-dark v-align-middle no-underline h4 js-navigation-open" href=${href}>${res}</a>] ${def}`;
     }
   }
 }
